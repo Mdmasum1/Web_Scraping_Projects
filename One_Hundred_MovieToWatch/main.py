4f3cf759b427ede9c75bc18657c45368f942e181
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2023-12-11 22:32:24 +0000

    have changed the code

--- One_Hundred_MovieToWatch/main.py
+++ One_Hundred_MovieToWatch/main.py
@@ -5,4 +5,14 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+#Print the soup beautifully
+#print(soup.prettify())
+all_movies = soup.find_all(name="h3", class_="title")
+print(all_movies)
+
 
