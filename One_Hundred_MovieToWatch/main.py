47c7d95557c207263e7b3abc8a9d2e3eb788f90b
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2023-12-12 19:54:50 +0000

    have added more code

--- One_Hundred_MovieToWatch/main.py
+++ One_Hundred_MovieToWatch/main.py
@@ -13,6 +13,24 @@ soup = BeautifulSoup(website_html, "html.parser")
 #Print the soup beautifully
 #print(soup.prettify())
 all_movies = soup.find_all(name="h3", class_="title")
-print(all_movies)
+#print(all_movies)
+
+#Using list comprehensions
+movie_titles = [movie.getText() for movie in all_movies]
+print(movie_titles[:: -1])
+
+movies = movie_titles[:: -1]
+
+# for n in range(len(movie_titles) -1, -1, -1):
+#     print(movie_titles[n])
+
+# with open("move.text", mode="w") as file:
+#     for movie in movies:
+
+#         file.write(f"{movie}\n")
+
+
+
+
 
 
