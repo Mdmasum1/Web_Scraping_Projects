c3e1c1c63bf80c9f65f317ed9a2d892736734b0f
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-08-08 13:45:38 +0000

    have changed the code

--- One_Hundred_MovieToWatch/child.py
+++ One_Hundred_MovieToWatch/child.py
@@ -2,15 +2,63 @@
 #If you want to find only descendants that are children, you can use 
 #the .children tag:
 
-import requests
+# import requests
+# from bs4 import BeautifulSoup
+
+# url = 'http://www.pythonscraping.com/pages/page3.html'
+# response = requests.get(url)
+# html = response.text
+# bs = BeautifulSoup(html, 'html.parser')
+
+# for child in bs.find('table', {'id' : 'giftList'}). children:
+#     print(child)
+
+#**************************************************************
+#The solution is to look for something identifying about the tag itself.
+#In this case, you can look at the file path of the product 
+#imgages
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
-url = 'http://www.pythonscraping.com/pages/page3.html'
-response = requests.get(url)
-html = response.text
+html = urlopen('http://www.pythonscraping.com/pages/page3/html')
 bs = BeautifulSoup(html, 'html.parser')
+images = bs.find_all('imag', 
+                     {'src': re.compile('\.\.\/img\/gifts/img.*\.jpg')})
 
-for child in bs.find('table', {'id' : 'giftList'}). children:
-    print(child)
+
+for image in images:
+    print(image['src'])
+
+'''
+ ---This prints only the relative image paths that start with 
+ ../img/gifts/img and end in .jpg, the output of which is the 
+ following :
+../img/gifts/img1.jpg
+ ../img/gifts/img2.jpg
+ ../img/gifts/img3.jpg
+ ../img/gifts/img4.jpg
+ ../img/gifts/img6.jpg
+
+##A regular expresion can be inserted as any argument in a BeautifulSoup
+ expression, allowing you a great deal of flexibility in finding target
+ elements
+ 
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
