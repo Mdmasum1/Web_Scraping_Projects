44f7a8732b8cc5ae0057930082de7d662b7baded
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-11-25 22:47:57 +0000

    have modified the code

--- 4_pr_soup_Snium_scrapy/Selenium_Pro.py
+++ 4_pr_soup_Snium_scrapy/Selenium_Pro.py
@@ -1,21 +1,32 @@
 
 from selenium import webdriver
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
+# URL to scrape
 url = 'https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/'
 
-# Replace with the actual path to ChromeDriver
-#chrome_driver_path = '/workspaces/Web_Scraping_Projects/chromedriver'
+# Path to ChromeDriver
+chrome_driver_path = '/usr/local/bin/chromedriver'
 
 # Create a Service object
-#service = Service(chrome_driver_path)
-driver = webdriver.Chrome(executable_path='/workspaces/Web_Scraping_Projects/chromedriver')
+service = Service(chrome_driver_path)
 
-#Open a web page 
+# Configure Chrome options
+options = Options()
+options.add_argument('--headless')  # Run Chrome in headless mode
+options.add_argument('--no-sandbox')  # Prevent sandbox issues
+options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
+options.add_argument('--disable-gpu')  # Disable GPU acceleration (optional for headless)
+
+# Initialize the WebDriver with options and service
+driver = webdriver.Chrome(service=service, options=options)
+
+# Open the web page
 driver.get(url)
 
-#print the page title
-print(driver)
+# Print the page title
+print("Page title is:", driver.title)
 
-#Close the browser
+# Close the browser
 driver.quit()
