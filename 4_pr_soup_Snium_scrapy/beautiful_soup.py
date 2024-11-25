40347e2e95013966a4005e34750be56ee4af4e79
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-11-25 13:09:06 +0000

    Have changed the code

--- 4_pr_soup_Snium_scrapy/beautiful_soup.py
+++ 4_pr_soup_Snium_scrapy/beautiful_soup.py
@@ -2,32 +2,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-#Beautiful Soup: Steps before scraping a website
+website = 'https://subslikescript.com/movie/Titanic-120338'
 
-o0#1)Fetch the pages(obtained a resonse object)
-result = requests.get("wwww.google.com")
+#Beautiful Soup: Steps before scraping a website
+#1)Fetch the pages(obtained a resonse object)
+result = requests.get(website)
 
 #2) Page content
-content = result.text
-
+content = result.text 
 
 #3) Create soup
 soup = BeautifulSoup(content, "lxml")
 
+print(soup.prettify())
 
 #Finding element with Beautiful Soup
-soup.find(id="specific_id")
+# soup.find(id="specific_id")
 
-#Taking two arguments
-soup.find('tag', class_="class value")
+# #Taking two arguments
+# soup.find('tag', class_="class value")
 
-#ex: tag is 'h1' here taking only one arguments
-soup.find('tag')
+# #ex: tag is 'h1' here taking only one arguments
+# soup.find('tag')
 
-#Fiding all elments with Beautiful Soup
-#find_ll return list -->list = [a, b, c, d]
+# #Fiding all elments with Beautiful Soup
+# #find_ll return list -->list = [a, b, c, d]
 
-soup.find_all("h2")
+# soup.find_all("h2")
 
 
 
