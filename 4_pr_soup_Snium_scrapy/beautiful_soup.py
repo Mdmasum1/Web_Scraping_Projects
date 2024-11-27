92ad75eb7f5515cc4e9131231cce37fd0269aa7c
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-11-27 03:43:38 +0000

    Have modified the code

--- 4_pr_soup_Snium_scrapy/beautiful_soup.py
+++ 4_pr_soup_Snium_scrapy/beautiful_soup.py
@@ -55,4 +55,12 @@ website = 'https://subslikescript.com/movie/Titanic-120338'
 response = scraper.get(website)
 
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
+#print(soup.prettify())
+
+title = soup.find('h1').get_text()
+transcript = soup.find('div', class_='full-script').get_text(strip=True, separator=' ')
+#print(transcript)
+
+with open(f'{title}.txt', 'w') as file:
+    file.write(transcript)
+
