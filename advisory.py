ec1ffdd348129e468f69f9f83fc3687b3f003291
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-11-30 23:17:02 +0000

    have modified the code

--- advisory.py
+++ advisory.py
@@ -28,6 +28,39 @@ soup = BeautifulSoup(page_content, 'html.parser')
 #here is tag for the name "<a href="http://www.rhbuchanan.com" rel="nofollow" target="_blank" style="color: #00b0f0;  text-decoration: none !important; line-height: 30px;">RH BUCHANAN</a>"
 #under h1 tag"
 # # Locate <h1> tag and then the <a> inside it
-name = soup.select_one("h1 a").text
+name = soup.find('h1').find('a').getText(strip=True)
  
-print(name)
+#print(name)
+
+#Extract the phone number
+#'
+'''  
+  <div style=" margin: 10px 0px 18px"> 
+  <div>Tel:  (907) 225-0833</div>
+    29 Main Street
+    <br>Suite 202<br>
+    <span>Ketchikan, Alaska </span>&nbsp;<span>99901</span>		
+</div>
+
+'''
+
+# Locate the div tag with the phone number
+telephone = soup.find('div', style=' margin: 10px 0px 18px').find('div').getText(strip=True)
+
+#Street adress only 
+''' 
+# <div style=" margin: 10px 0px 18px"> 
+  <div>Tel:  (907) 225-0833</div>
+    29 Main Street"
+'''
+# Locate the div tag with the street address
+#not the telephone number only the street address
+street = soup.find('div', style=' margin: 10px 0px 18px').find('div').find_next_sibling().getText(strip=True)
+
+# Display results
+print(f"Name: {name}")
+print(f"Telephone: {telephone}")
+print(f"Street: {street}")
+# print(f"Suite: {suite}")
+# print(f"City, State: {city_state}")
+# print(f"ZIP Code: {zip_code}")
