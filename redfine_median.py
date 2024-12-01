8527bdd226d81c1f1aeb22b331c01d3aee809e17
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-12-01 12:02:32 +0000

    have changed the code

--- redfine_median.py
+++ redfine_median.py
@@ -24,7 +24,12 @@ def scrape_median_sale_prices_selenium(url):
 
         # Wait for the chart element to be present
         wait = WebDriverWait(driver, 10)
-        chart_element = wait.until(EC.presence_of_element_located((By.XPATH, '//div[contains(@id, "medianSalePriceChart")]')))
+        #<div class="housingMarketDropdown" id="home_prices"><span class="field select Select withFlyout withOptions mounted clickable optional" data-rf-test-name="Select"><span data-rf-test-name="Select" class="input" role="listbox" tabindex="0"><span class="container"><span class="value">Median Sale Price</span><span class="arrow"><svg class="SvgIcon dropdown"><svg viewBox="0 0 10 10"><path d="M1.601 3h6.792a.25.25 0 01.177.427L5.174 6.823a.25.25 0 01-.354 0L1.424 3.427A.25.25 0 011.601 3"></path></svg></svg></span></span><select class="select" tabindex="-1"><option selected="" value="0" aria-label="Median Sale Price">Median Sale Price</option><option value="1" aria-label="# of Homes Sold"># of Homes Sold</option><option value="2" aria-label="Median Days on Market">Median Days on Market</option></select></span></span><span class="field select Select propertyTypeDropdown withFlyout withOptions mounted selected clickable optional" data-rf-test-name="Select"><span data-rf-test-name="Select" class="input" role="listbox" tabindex="0"><span class="container"><span class="value">All Home Types</span><span class="arrow"><svg class="SvgIcon dropdown"><svg viewBox="0 0 10 10"><path d="M1.601 3h6.792a.25.25 0 01.177.427L5.174 6.823a.25.25 0 01-.354 0L1.424 3.427A.25.25 0 011.601 3"></path></svg></svg></span></span><select class="select" tabindex="-1"><option selected="" value="All" aria-label="All Home Types">All Home Types</option><option value="6" aria-label="Single Family Homes">Single Family Homes</option><option value="13" aria-label="Townhouses">Townhouses</option><option value="3" aria-label="Condos/Co-ops">Condos/Co-ops</option></select></span></span></div>
+        #show me the median sale price and Xpath
+        #Xpath
+        
+
+        chart_element = wait.until(EC.presence_of_element_located((By.XPATH, '//div[(@id, "home-prices")]')))
         
         # Extract chart data
         chart_data = chart_element.get_attribute('data-chart-data')
