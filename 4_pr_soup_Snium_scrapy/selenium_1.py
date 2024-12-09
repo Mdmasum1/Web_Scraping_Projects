23250944afa1e9330ff8fdde654e7d0691c69571
mdbillah <18408907+Mdmasum1@users.noreply.github.com>
2024-12-09 12:14:06 +0000

    have changed the code

--- 4_pr_soup_Snium_scrapy/selenium_1.py
+++ 4_pr_soup_Snium_scrapy/selenium_1.py
@@ -1,22 +1,29 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-
-#url of the page we want to scrape
-web ="https://www.amazon.com"
+# URL of the page we want to scrape
+web = "https://www.amazon.com"
 
 # Path to the chromedriver
 path = "/usr/local/bin/chromedriver"
 
-#create new instances of service
-Service = Service(executable_path=path)
+# Create a new instance of Options
+chrome_options = Options()
+
+# Create new instances of service
+service = Service(executable_path=path)
+
 
 # Create a new instance of the Chrome driver as driver
-driver = webdriver.Chrome(service=Service)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Navigate to the page by driver
+# Navigate to the page
 driver.get(web)
 
-#quit driver
-#driver.quit()
+# Print the page title to verify
+print("Page title is:", driver.title)
+
+# Close the browser
+driver.quit()
